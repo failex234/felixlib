3e5a2141b03698fca13044f363accae7f4cd2fca
Felix Naumann <felix@emilnaumann.de>
2018-02-25 21:41:42 +0100

    base commit

---
+++ src/felixlib.c
@@ -0,0 +1,126 @@
+#include "felixlib.h"
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * get size of a string
+ */
+int getsize(const char *string) {
+	int ch;
+	int i = 0;
+	while ((ch = string[i]) != '\0') {
+		i++;
+	}
+
+	return i - 1;
+}
+
+/*
+ * calculate base to the power of exp
+ */
+int pow(const int base, int exp) {
+	int result = 1;
+	while(exp != 0) {
+		result *= base;
+		exp--;
+	}
+
+	return result;
+}
+
+/* Convert string to int by firstly
+ * getting the single number on any position by subtracting 48 (or in this case '0')
+ * then multiplying it by the potency of the numberbase, based on the position
+ * and then adding it to the result
+ *
+ * example:
+ * input: 120 (base 10)
+ *
+ * tempnum = 1
+ * finalnum = 1 * 10^(stringsize - i - 1)
+ * finalnum = 1 * 10^2
+ * finalnum = 100
+ *
+ * result = 100
+ *
+ * tempnum = 2
+ * finalnum = 2 * 10^(stringsize - i - 1)
+ * finalnum = 2 * 10^1
+ * finalnum = 20
+ *
+ * result = 100 + 20
+ * result = 120
+ *
+ * tempnum = 0;
+ * finalnum = 0 * 10^(stringsize - i - 1)
+ * finalnum = 0 * 10^0
+ * finalnum = 0;
+ *
+ * result = 120 + 0
+ *
+ * result = 120
+ *
+ */
+int toint(const char *string) {
+
+	int result = 0;
+	int tempnum = 0;
+	int finalnum = 0;
+	for (int i = 0; i < getsize(string); i++) {
+		tempnum = string[i] - '0';
+		finalnum = tempnum * pow(10, getsize(string) - i - 1);
+		result += finalnum;
+	}
+
+	return result;
+
+}
+
+//Still figuring out how to return the split string. Maybe as an struct array
+char **strsplit(const char *inputstring, const char *splitchar) {
+	char **splitarr = NULL;
+	int size = 0;
+	int bufpos = 0;
+	int splitarrsize = 0;
+	int splitarrpos = 0;
+	int endsize = 0;
+	char *buf = malloc(sizeof(char) * 1024);
+	for(int i = 0; i < getsize(inputstring); i++) {
+		if (inputstring[i] == splitchar) {
+			if (splitarr == 0) {
+				splitarr = malloc(sizeof(char) * getsize(buf));
+				splitarr[0] = malloc(sizeof(char) * getsize(buf));
+				strcpy(splitarr[splitarrpos], buf);
+				splitarrsize = getsize(buf);
+				splitarrpos++;
+				endsize++;
+				
+				size = splitarrsize;
+				//omit splitchar
+				i++;
+			} else {
+				splitarr = realloc(splitarr, sizeof(char) * (size + getsize(buf)));
+				splitarr[splitarrpos] = malloc(sizeof(char) * getsize(buf));
+				strcpy(splitarr[splitarrpos], buf);
+				splitarrpos++;
+				endsize++;
+				
+				size += getsize(buf);
+			}
+			free(buf);
+			buf = malloc(sizeof(char) * 1024);
+			bufpos = 0;
+		} else {
+			buf[bufpos] = inputstring[i];
+			bufpos++;
+		}
+	}
+	
+	splitarr = realloc(splitarr, sizeof(char) * (size + getsize(buf)));
+	splitarr[splitarrpos] = malloc(sizeof(char) * getsize(buf));
+	strcpy(splitarr[splitarrpos], buf);
+	free(buf);
+	endsize++;
+	
+	return endsize;
+}
---
+++ src/felixlib.h
@@ -0,0 +1,31 @@
+#ifndef FLX_STDLIB
+#define FLX_STDLIB
+
+/*
+ * Covert a string containing numbers to an int
+ * "1337" -> 1337
+ */
+int toint(const char*);
+
+/*
+ * Calculate powers of ints
+ */
+int pow(const int, int);
+
+/*
+ * Get size of a string
+ */
+int getsize(const char*);
+
+/*
+ * syntax strplit(splitarr, inputstring, splitchar)
+ * splits inputstring every time splitchar is found
+ * inside the string.
+ * saves result in splitarr, returns number of array elements
+ * 
+ * inputstring = "Hello, World, Hello!", splitchar = ','
+ * splitarr = ["Hello", " World", " Hello!"]
+ */
+char **strsplit(const char*, const char*); 
+
+#endif
